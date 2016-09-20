44cc955209d1f55ca5fb145b0438df583360e741
TheGman117 <gman117@gmail.com>
2016-09-19 20:11:49 -0400

    Removed the return line from each if/else if statement
    Removed return line from each if statement. Had been left there from an attempt to use a loop for input

--- CutterCalculator.py
+++ CutterCalculator.py
@@ -13,21 +13,18 @@ if CalcType == "IPM":
 
     IPM = FPT * Teeth * RPM
     print (IPM)
-    return
 elif CalcType == "RPM":
     IPM = int(input("Please input desired IPM: "))
     WOC = float(input("Please input width of cut at desired depth: "))
 
     RPM = (IPM * 1000) / (3.14 * WOC)
     print (RPM)
-    return
 elif CalcType == "SFM":
     RPM = int(input("Please input RPM: "))
     WOC = float(input("Please input width of cut at desired depth: "))
 
     SFM = WOC * 0.2618 * RPM
     print (SFM)
-    return
 elif CalcType == "FPT":
     IPM = int(input("Please input desired IPM: "))
     Teeth = int(input("Please input amount of teeth on cutter: "))
@@ -35,7 +32,5 @@ elif CalcType == "FPT":
 
     FPT = IPM / (Teeth * RPM)
     print (FPT)
-    return
 else:
     print ("That is not a valid choice")
-    return
