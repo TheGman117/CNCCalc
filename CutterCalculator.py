ad1e76910ceeaa75747f0474f5af7dcecfc4309a
TheGman117 <gman117@gmail.com>
2016-09-19 20:28:00 -0400

    Added Efficiency calculation option

--- CutterCalculator.py
+++ CutterCalculator.py
@@ -32,5 +32,21 @@ elif CalcType == "FPT":
 
     FPT = IPM / (Teeth * RPM)
     print (FPT)
+    elif CalcType = "Efficiency comparison":
+    IPM1 = int(input("Please input IPM of cutter 1: "))
+    DOC1 = float(input("Please input depth of cut of cutter 1: "))
+
+    IPM2 = int(input("Please input IPM of cutter 2: "))
+    DOC2 = float(input("Please input depth of cut of cutter 2: "))
+
+    MaterialPerMinute1 = IPM1 * DOC1
+    MaterialPerMinute2 = IPM2 * DOC2
+
+    if MaterialPerMinute1 > MaterialPerMinute2:
+        Difference = MaterialPerMinute1 - MaterialPerMinute2
+        print ("Cutter 1 is more efficient by " Difference " cubic inches per minute.")
+    elif MaterialPerMinute1 < MaterialPerMinute2:
+        Difference = MaterialPerMinute2 - MaterialPerMinute1
+        print ("Cutter 2 is more efficient by " Difference " cubic inches per minute.")
 else:
     print ("That is not a valid choice")
